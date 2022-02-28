87bc6ca6170f3082f0f3db99902f29b081280537
mauchaconrithmXO <Mauricio.Chacon@rithmXO.com>
2022-02-28 10:47:59 -0400

    fix

--- routes/web/role.php
+++ routes/web/role.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     // routes user
-    Route::get('/{rol}/assign_permissions', [App\Http\Controllers\RoleController::class, 'assign_permissions'])->name('roles.permissions');
-    Route::put('/{rol}/insert_permissions', [App\Http\Controllers\RoleController::class, 'insert_permissions'])->name('roles.insert-permissions');
+    Route::get('/{rol}/assign-permissions', [App\Http\Controllers\RoleController::class, 'assign_permissions'])->name('roles.permissions');
+    Route::put('/{rol}/insert-permissions', [App\Http\Controllers\RoleController::class, 'insert_permissions'])->name('roles.insert_permissions');
 });
 
 
